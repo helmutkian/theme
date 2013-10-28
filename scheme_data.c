7d3d1178f402ec5ae966a5c432bef87d152f0764
helmutkian <helmut.rohrbacher@gmail.com>
2013-10-28 10:08:00 -0700

    Implemented ratio and bool types. Implemented generic 'constructor' for heap-allocated data. Implemented print function for cons cells.

--- scheme_data.c
+++ scheme_data.c
@@ -1,39 +1,55 @@
 #include "scheme_data.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 static void _print_fixnum(scheme_data *);
 static void _print_float(scheme_data *);
 static void _print_char(scheme_data *);
 static void _print_string(scheme_data *);
-// static void _print_cons(scheme_data *);
+static void _print_cons(scheme_data *);
+static void _print_nil(scheme_data *);
 static void _print_symbol(scheme_data *);
+static void _print_bool(scheme_data *);
+static void _print_ratio(scheme_data *)
 
 static void _free_string(scheme_data *);
 static void _free_symbol(scheme_data *);
 
+scheme_data *scheme_alloc_data(scheme_type type)
+{
+  scheme_data *data = malloc(size_of(scheme_data));
+  assert(data != NULL);
+  data->type = type;
+  return data;
+}
+
 void scheme_print_data(scheme_data *data)
 {
   switch (data->type) {
-  case FLOAT:
+  case SCHEME_FLOAT:
     _print_float(data);
     break;
-  case FIXNUM:
+  case SCHEME_FIXNUM:
     _print_fixnum(data);
     break;
-  case CHAR:
+  case SCHEME_CHAR:
     _print_char(data);
     break;
-  case STRING:
+  case SCHEME_STRING:
     _print_string(data);
     break;
-  case CONS:
+  case SCHEME_CONS:
     break;
-  case NIL:
-    printf("()");
-  case SYMBOL:
+  case SCHEME_NIL:
+    _print_nil(data);
+  case SCHEME_SYMBOL:
     _print_symbol(data);
     break;
+  case SCHEME_BOOL:
+    _print_bool(data);
+  case SCHEME_RATIO:
+    _print_ratio(data);
   }
 }
 
@@ -60,30 +76,63 @@ void _print_string(scheme_data *str_data)
   }
 }
 
+void _print_cons(scheme_data *cons_data)
+{
+  scheme_data *cell;
+
+  for (cell = cons_data
+     ; cell->type != SCHEME_NULL
+     ; cell = cell->cons.cdr) {
+    scheme_print_data(cell->cons.car);
+    if (cell->cons.cdr->type != SCHEME_CONS) {
+      scheme_print_data(cell->cons.cdr);
+      break;
+    }
+  }
+}
+
+void _print_nil(scheme_data *str_data)
+{
+  printf("()");
+}
+
 void _print_symbol(scheme_data *sym_data)
 {
   printf("%s", sym_data->sym);
 }
 
+void _print_bool(scheme_data *bool_data)
+{
+  printf("#%c", bool_data->b ? 't' : 'f');
+}
+
+void _print_ratio(scheme_data *ratio_data)
+{
+  printf("%d/%d", ratio_data->r.num, ratio_data->r.denom);
+}
 
 void scheme_free_data(scheme_data *data)
 {
   switch (data->type) {
-  case FLOAT:
+  case SCHEME_FLOAT:
     break;
-  case FIXNUM:
+  case SCHEME_FIXNUM:
     break;
-  case CHAR:
+  case SCHEME_CHAR:
     break;
-  case STRING:
+  case SCHEME_STRING:
     _free_string(data);
     break;
-  case CONS:
+  case SCHEME_CONS:
     break;
-  case NIL:
+  case SCHEME_NIL:
     break;
-  case SYMBOL:
+  case SCHEME_SYMBOL:
     _free_symbol(data);
+    break;
+  case SCHEME_BOOL:
+    break;
+  case SCHEME_RATIO:
     break;
   }
 
--- scheme_data.h
+++ scheme_data.h
@@ -2,13 +2,15 @@
 #define SCHEME_DATA_H
 
 typedef enum {
-  FLOAT
-  , FIXNUM
-  , CHAR
-  , STRING
-  , CONS
-  , NIL
-  , SYMBOL
+  SCHEME_FLOAT
+  , SCHEME_FIXNUM
+  , SCHEME_CHAR
+  , SCHEME_STRING
+  , SCHEME_CONS
+  , SCHEME_NIL
+  , SCHEME_SYMBOL
+  , SCHEME_BOOL
+  , SCHEME_RATIO
 } scheme_type;
 
 typedef struct {
@@ -16,21 +18,29 @@ typedef struct {
   int length;
 } scheme_string;
 
+typedef struct {
+  int num;
+  int denom;
+} scheme_ratio;
+
 typedef struct _scheme_data {
   scheme_type type;
-  union {
-    double f;
-    int i;
-    char c;
-    char *sym;
-    scheme_string str;
+  union {  
     struct {
       struct _scheme_data *car;
       struct _scheme_data *cdr;
-    } cons;
+    } cons;                     // SCHEME_CONS
+    double f;                   // SCHEME_FLOAT
+    int i;                      // SCHEME_FIXNUM
+    char c;                     // SCHEME_CHAR
+    char *sym;                  // SCHEME_SYMBOL
+    scheme_string str;          // SCHEME_STR
+    unsigned char b;            // SCHEME_BOOL
+    scheme_ratio r;             // SCHEME_RATIO    
   };
 } scheme_data;
 
+scheme_data *scheme_alloc_data(scheme_type type);
 void scheme_print_data(scheme_data *data);
 void scheme_free_data(scheme_data *data);
 
