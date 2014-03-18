0f55c7ddd1f97b67df2168f8a1c467db3a679cff
Helmut K Rohrbacher <helmut.rohrbacher@gmail.com>
2014-03-17 18:05:19 -0700

    Fixed tests to work with new functions

--- test.c
+++ test.c
@@ -19,9 +19,9 @@
 // Reader test constructor
 // ************************************************************
 
-void test_reader(char *test_name, Scm_Reader reader, char *expected, Scm_Printer printer)
+void test_reader(char *test_name, reader reader, char *expected, printer printer)
 {
-  Scm_Value val;
+  struct value val;
   FILE *in;
 
   in = fmemopen(expected, strlen(expected), "r");
@@ -29,7 +29,7 @@ void test_reader(char *test_name, Scm_Reader reader, char *expected, Scm_Printer
   puts(test_name);
 
 
-  if (reader(in, &val)) {
+  if (!reader(in, &val)) {
     printf("\tExpected: %s\n", expected);
     printf("\tSaw: ");
     printer(stdout, &val);
@@ -46,8 +46,8 @@ void test_reader(char *test_name, Scm_Reader reader, char *expected, Scm_Printer
 // Reader test
 // ************************************************************
 
-DEF_READ_TEST(integer);
-DEF_READ_TEST(real);
+DEF_READ_TEST(fixnum);
+DEF_READ_TEST(flonum);
 DEF_READ_TEST(character);
 DEF_READ_TEST(string);
 
@@ -58,15 +58,15 @@ DEF_READ_TEST(string);
 int main()
 {
 
-  RUN_READ_TEST(integer, "1");
-  RUN_READ_TEST(integer, "-3");
-  RUN_READ_TEST(integer, "121");
-  RUN_READ_TEST(integer, "-772");
+  RUN_READ_TEST(fixnum, "1");
+  RUN_READ_TEST(fixnum, "-3");
+  RUN_READ_TEST(fixnum, "121");
+  RUN_READ_TEST(fixnum, "-772");
 
-  RUN_READ_TEST(real, "1.121");
-  RUN_READ_TEST(real, "-65.23");
-  RUN_READ_TEST(real, ".22");
-  RUN_READ_TEST(real, "+.12");
+  RUN_READ_TEST(flonum, "1.121");
+  RUN_READ_TEST(flonum, "-65.23");
+  RUN_READ_TEST(flonum, ".22");
+  RUN_READ_TEST(flonum, "+.12");
   
   RUN_READ_TEST(character, "#\\a");
   RUN_READ_TEST(character, "#\\1");
