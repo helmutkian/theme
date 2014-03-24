49f9ad85a556c4ae830b5980542e09e5b66a3997
Helmut K Rohrbacher <helmut.rohrbacher@gmail.com>
2014-03-23 18:11:15 -0700

    Rewriting 'reader' as lexer+parser

---
+++ lexer.c
@@ -0,0 +1,162 @@
+#include "lexer.h"
+#include <ctype.h>
+
+#define cur(lexer_) ##lexer_->buf[##lexer_->pos]
+
+static int _is_delim(char c);
+static Scm_Token _find_start_pos(Scm_Lexer *lexer);
+static Scm_Token _lex_string(Scm_Lexer *lexer);
+static Scm_Token _lex_character(Scm_Lexer *lexer);
+
+int _is_delim(char c)
+{
+  char delims[] = "();";
+
+  return isspace(c) || (EOF == c) || strchr(delims, c);
+}
+ 
+// Advances the position of the lexer in its buffer beyond any
+// leading whitespace or comments.
+//
+// Returns TOKEN_ERROR if comments are misformatted, !TOKEN_ERROR
+// otherwise.
+Scm_Token _find_start_pos(Scm_Lexer *lexer)
+{
+  while (cur(lexer)) {
+    if (';' == cur(lexer)) { // Endline comments
+      do {
+	lexer->pos++;
+	// Endline comment must end in newline
+	if (!lexer(cur)) 
+	  return TOKEN_ERROR;
+      } while ('\n' != cur(lexer));
+    } else if ('#' == cur(lexer)) { // Block comments
+      lexer->pos++;
+      if ('|' == cur(lexer)) {
+	// Look for comment block end: "|#"
+	for (;;) {
+	  lexer->pos++;
+	  if ('|' == cur(lexer)) {
+	    lexer->pos++;
+	    if ('#' == cur(lexer)) {
+	      break;
+	    } else {
+	      lexer->pos--;
+	    }
+	  } else if (!cur(lexer)) { // EOF before block end => ERROR
+	    return TOKEN_ERROR;
+	  }
+	}
+      }
+    } else if (isspace(cur(lexer))) { // Whitespace
+      lexer->pos++;
+    } else {
+      return !TOKEN_ERROR;
+    }
+  }
+}
+
+Scm_Token _lex_string(Scm_Lexer *lexer)
+{
+  int i;
+  lexer->lexeme = Scm_alloc(TYPE_STRING);
+
+  for (i = 0; cur(lexer) != '"'; i++) {
+    lexer->pos++;
+    if ('\\' == cur(lexer)) { // Handle escape chars
+      lexer->pos++;
+      if ('n' == tolower(curr(lexer))) 
+	lexer->lexeme->string[i] = '\n';
+      else if ('t' == tolower(curr(lexer)))
+	lexer->lexeme->string[i] = '\t';
+      else if ('\\' == curr(lexer)) 
+	lexer->lexeme->string[i] = '\\';
+      else 
+	goto err;
+    } else if (!curr(lexer)) { // No close quote
+      goto err;
+    } else {
+      lexer->lexeme->string[i] = curr(lexer);
+    }
+  }
+	
+  return TOKEN_LITERAL;
+
+ err:
+  Scm_free(lexer->lexeme);
+  return TOKEN_ERROR;
+}
+
+Scm_Token _lex_character(Scm_Lexer *lexer)
+{
+  int i = 1; // Position offset of ensuring delimiter after char literal
+  char c, named_char;
+  char char_name[15] = {0};
+
+  lexer->lexeme = Scm_alloc(TYPE_CHARACTER);
+
+  switch (tolower(curr(lexer))) {
+  case 'n':
+    strcpy(char_name, "newline");
+    named_char = '\n'
+    break;
+  case 't':
+    strcpy(char_name, "tab");
+    named_char = '\t'
+    break;
+  case 's':
+    strcpy(char_name, "space");
+    named_char = ' ';
+    break;
+  default: 
+    break;
+  }
+
+  if (!char_name[0]) {
+    lexer->lexeme->character = curr(lexer);
+  } else { 
+    for (i = 0; !char_name[i]; i++) {
+      c = lexer->buf[lexer->pos + i];
+      if (!c || (tolower(c) != char_name[i])) 
+	goto err;
+    }
+    lexer->lexeme->character = named_char;
+  }
+
+  if (_is_delim(lexer->buf[lexer->pos + i]))
+    return TOKEN_LITERAL;
+
+ err:
+  Scm_free(lexer->lexeme);
+  return TOKEN_ERROR;
+}
+
+    
+
+Scm_Token Scm_lex(Scm_Lexer *lexer)
+{
+  if (_find_start_pos(lexer) == TOKEN_ERROR)
+    return TOKEN_ERROR;
+
+  if (EOF == cur(lexer)) 
+    return TOKEN_EOF;
+
+  switch (lexer->buf[lexer->pos++]) {
+  case '.': return TOKEN_DOT;
+  case '(': return TOKEN_OPEN_PAREN;
+  case ')': return TOKEN_CLOSE_PAREN;
+  case '"': return _lex_string(lexer);
+  case '#':
+    if ('\\' == cur(lexer)) 
+      return _lex_character(lexer);
+    else 
+      return _lex_boolean(lexer);
+  default: 
+    lexer->position--;
+    break;
+  }
+
+}
+      
+ 
+    
---
+++ lexer.h
@@ -0,0 +1,20 @@
+#ifndef _LEXER_H_
+#define _LEXER_H_
+
+typedef enum {
+    TOKEN_ERROR
+  , TOKEN_EOF
+  , TOKEN_SEMICOLON
+  , TOKEN_DOT
+  , TOKEN_OPEN_PAREN
+  , TOKEN_CLOSE_PAREN
+  , TOKEN_LITERAL
+} Scm_Token;
+
+typedef struct {
+  char *buf;
+  unsigned int pos;
+  Scm_Value *lexeme;
+} Scm_Lexer;
+
+#endif
